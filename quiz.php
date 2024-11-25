9509ff4655f5eee94e5d9b3d292107f196a992d2
MariaRenaBandiala <202280052@psu.palawan.edu.ph>
2024-11-26 00:28:08 +0800

    quiz

---
+++ quiz.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+// Retrieve settings from the form submission
+$operation = $_POST['operation']; // The selected operation ( add, subtract, multiply,and divide.)
+$level = $_POST['level'];         // Difficulty level (1 for easy, 2 for hard)
+$questions = $_POST['questions']; // Number of quiz questions ( 10, 20, 30)
