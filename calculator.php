925ebda8d6ef94755aeddfb6917138b1b5358c45
MariaRenaBandiala <202280052@psu.palawan.edu.ph>
2024-11-25 21:30:30 +0800

    Addition

--- calculator.php
+++ calculator.php
@@ -12,5 +12,19 @@
         <input type="number" name="num2" placeholder="Enter second number" required>
         <button type="submit" name="calculate">Add</button>
     </form>
+
+    <?php
+    if (isset($_POST['calculate'])) {
+       
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+
+       
+        $result = $num1 + $num2;
+
+        
+        echo "<h2>Result: $result</h2>";
+    }
+    ?>
 </body>
 </html>
