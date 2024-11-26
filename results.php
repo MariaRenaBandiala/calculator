c8b2d05d6fbd50d189a62d36c771727c28d2a62c
MariaRenaBandiala <202280052@psu.palawan.edu.ph>
2024-11-26 09:10:47 +0800

    Added operation for calculation

--- results.php
+++ results.php
@@ -6,3 +6,30 @@ $quiz = $_SESSION['quiz'];
 $operation = $_SESSION['operation'];
 $answers = $_POST['answers'];
 $totalQuestions = count($quiz);
+
+// Calculate correct answers
+$correct = 0;
+foreach ($quiz as $index => $q) {
+    $num1 = $q['num1'];
+    $num2 = $q['num2'];
+
+    switch ($operation) {
+        case 'add':
+            $expected = $num1 + $num2;
+            break;
+        case 'subtract':
+            $expected = $num1 - $num2;
+            break;
+        case 'multiply':
+            $expected = $num1 * $num2;
+            break;
+        case 'divide':
+            $expected = $num2 != 0 ? $num1 / $num2 : 0; // not divide by
+            break;
+    }
+
+    if (round($expected) == round($answers[$index])) {
+        $correct++;
+    }
+}
+
